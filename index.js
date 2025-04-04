df98cf3731785bc8aca6e38354b60f0be55baf78
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-04-04 14:12:40 +0200

    index.js commentaries

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { app } = require("./config/app");                                    // O
 const { usersRouter } = require("./controllers/users.controllers");         // On importe le UserRouter que l'on a plus tôt exporté depuis books.controller. 
 const { booksRouter } = require("./controllers/books.controllers");         // On importe le booksRouter que l'on a plus tôt exporté depuis books.controller. 
 
-const PORT = process.env.PORT || 4000;                                      // On définit le port sur lequel le serveur va écouter les requêtes. Si la variable d'environnement PORT n'est pas définie, il utilisera le port 4000 par défaut.
+const PORT = process.env.PORT || 4000;                                      // On définit le port sur lequel le serveur va écouter les requêtes. Si la variable d'environnement PORT n'est pas définie, alors il utilisera le port 4000 par défaut.
 
 app.get("/", (req, res) => res.send("Server is running !"));                // On définit une route GET pour la racine du serveur ("/"). Lorsque cette route est appelée, elle renvoie la réponse "Server is running !". Cela permet de vérifier que le serveur fonctionne correctement.
 app.use("/api/auth", usersRouter);                                          // On utilise le UserRouter pour gérer les requêtes liées à l'authentification des utilisateurs. Toutes les routes définies dans le UserRouter seront préfixées par "/api/auth".
