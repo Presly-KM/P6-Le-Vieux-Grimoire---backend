96f3a6018fb057fad50ba0faec55f37b3dc7cf3c
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-04-04 15:19:05 +0200

    Book.js commentaries

--- models/Book.js
+++ models/Book.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose");                     // On importe mongoose qui est un ODM (Object Document Mapper) pour MongoDB. Il permet de manipuler les données de la base de données MongoDB en utilisant des objets JavaScript. Il facilite la création, la lecture, la mise à jour et la suppression de documents dans une base de données MongoDB.
 
-const BookSchema = new mongoose.Schema({                  // Gestion ici d'un model/schéma d'affichage de la légende (ou description) des livres affichés. Voir la partie "Models" du pdf des instructions données pas Openclassrooms pour vérifier quel shéma de description il faut respecter. On veut aussi enregistrer les différends contenus de books (userId,title, autohor, yearn genre, ratinges, averageRating) dans la base de données. Il s'afiit ici de la represnetaiton en javascript d"une entité en base de donnée 
+const BookSchema = new mongoose.Schema({                  // Gestion ici d'un model/schéma d'affichage de la légende (ou description) des livres affichés. Voir la partie "Models" du pdf des instructions données pas Openclassrooms pour vérifier quel shéma de description il faut respecter. On veut aussi enregistrer les différends contenus de books (userId,title, author, year,genre, ratings, averageRating) dans la base de données. Il s'agit ici de la representation en Javascript d'une entité en base de donnée. 
     userId: String,
     title: String,
     author: String,
@@ -16,6 +16,6 @@ const BookSchema = new mongoose.Schema({                  // Gestion ici d'un mo
     averageRating: Number
   });
   
-  const Book = mongoose.model("Book", BookSchema);       // le const book vient rendre BookSchema (ci-dessus) opérationnel. Le code ci contre signifie que avec la const Book que mongoose  fabriquer une base de donnée "Book" (le 1er argument) en utilisant le BookSchema (2eme argument) 
+  const Book = mongoose.model("Book", BookSchema);       // le const book vient rendre BookSchema (ci-dessus) opérationnel. La constance Book avec l'aide de mongoose va fabriquer une base de donnée "Book" (le 1er argument) en utilisant le BookSchema (2eme argument) 
 
   module.exports = { Book };                             // On exporte ce nouveau schéma contenu dans "Book.js" pour l'importer dans "books.controller.js"
